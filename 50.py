c305d4ba534106471ce2895e613cdae8cdba20d0
unknown <C110156120@nkust.edu.tw>
2022-05-22 17:12:56 +0800

    first commit

---
+++ 50.py
@@ -0,0 +1,6 @@
+all = set(['John','Mary','Tina','Fiona','Clair','Eva','Ben','Bill','Bert'])
+english = set(['John','Mary','Fiona','Clair','Ben','Bill'])
+math = set(['Mary','Fiona','Clair','Eva','Ben'])
+print("英文與數學都及格",english&math)
+print("數學不及格",all-math)
+print("英文及格且數學不及格",english&(all-math))
