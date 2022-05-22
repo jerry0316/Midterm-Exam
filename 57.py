4ac29c998e0e329f8392a6dbfe06c4cfcd6a890e
unknown <C110156120@nkust.edu.tw>
2022-05-22 17:13:35 +0800

    first commit

---
+++ 57.py
@@ -0,0 +1,118 @@
+ques1=str(input("請選擇主餐及升級的套餐:"))
+ques2=str(input("是否升級成大杯飲料:"))
+ques3=str(input("是否換成大薯:"))
+
+if ques1=="1A":
+    if ques2=="是":
+        if ques3=="是":
+            print("總共為",72+55+7+13,"元")
+        else:
+            print("總共為",72+55+7,"元")
+    else:
+        if ques3=="是":
+            print("總共為",72+55+13,"元")
+        else:
+            print("總共為",72+55,"元")
+elif ques1=="1B":
+    if ques2=="是":
+        if ques3=="是":
+            print("總共為",72+68+7+13,"元")
+        else:
+            print("總共為",72+68+7,"元")
+    else:
+        if ques3=="是":
+            print("總共為",72+68+13,"元")
+        else:
+            print("總共為",72+68,"元")
+
+if ques1=="2A":
+    if ques2=="是":
+        if ques3=="是":
+            print("總共為",62+55+7+13,"元")
+        else:
+            print("總共為",62+55+7,"元")
+    else:
+        if ques3=="是":
+            print("總共為",62+55+13,"元")
+        else:
+            print("總共為",62+55,"元")
+elif ques1=="2B":
+    if ques2=="是":
+        if ques3=="是":
+            print("總共為",62+68+7+13,"元")
+        else:
+            print("總共為",62+68+7,"元")
+    else:
+        if ques3=="是":
+            print("總共為",62+68+13,"元")
+        else:
+            print("總共為",62+68,"元")
+
+if ques1=="3A":
+    if ques2=="是":
+        if ques3=="是":
+            print("總共為",82+55+7+13,"元")
+        else:
+            print("總共為",82+55+7,"元")
+    else:
+        if ques3=="是":
+            print("總共為",82+55+13,"元")
+        else:
+            print("總共為",82+55,"元")
+elif ques1=="3B":
+    if ques2=="是":
+        if ques3=="是":
+            print("總共為",82+68+7+13,"元")
+        else:
+            print("總共為",82+68+7,"元")
+    else:
+        if ques3=="是":
+            print("總共為",82+68+13,"元")
+        else:
+            print("總共為",82+68,"元")
+
+if ques1=="4A":
+    if ques2=="是":
+        if ques3=="是":
+            print("總共為",44+55+7+13,"元")
+        else:
+            print("總共為",44+55+7,"元")
+    else:
+        if ques3=="是":
+            print("總共為",44+55+13,"元")
+        else:
+            print("總共為",44+55,"元")
+elif ques1=="4B":
+    if ques2=="是":
+        if ques3=="是":
+            print("總共為",44+68+7+13,"元")
+        else:
+            print("總共為",44+68+7,"元")
+    else:
+        if ques3=="是":
+            print("總共為",44+68+13,"元")
+        else:
+            print("總共為",44+68,"元")
+
+if ques1=="5A":
+    if ques2=="是":
+        if ques3=="是":
+            print("總共為",60+55+7+13,"元")
+        else:
+            print("總共為",60+55+7,"元")
+    else:
+        if ques3=="是":
+            print("總共為",60+55+13,"元")
+        else:
+            print("總共為",60+55,"元")
+elif ques1=="5B":
+    if ques2=="是":
+        if ques3=="是":
+            print("總共為",60+68+7+13,"元")
+        else:
+            print("總共為",60+68+7,"元")
+    else:
+        if ques3=="是":
+            print("總共為",60+68+13,"元")
+        else:
+            print("總共為",60+68,"元")
