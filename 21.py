8bc412694533361b9f124cfd67abdfc5f7d986d2
unknown <C110156120@nkust.edu.tw>
2022-05-22 17:09:42 +0800

    first commit

---
+++ 21.py
@@ -0,0 +1,3 @@
+information={"123":["Tom","DTGD"],"456":["Cat","CSIE"],"789":["Nana","ASIE"],"321":["Lim","DBA"],"654":["Won","FDD"]}
+number=input("輸入學號:")
+print("學號:",number,"姓名:",information[number][0],"系別:",information[number][1])
